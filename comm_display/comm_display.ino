93ae30f7408177e1ee238da5855291238614a3fd
Alex Cruikshank <alex@carbonfive.com>
2014-11-20 18:18:22 -0500

    Added ability to 'send' LEDs with enter key

--- comm_display/comm_display.ino
+++ comm_display/comm_display.ino
@@ -12,6 +12,9 @@
 #define KEYBOARD_DATA_PIN 8
 #define KEYBOARD_IRQ_PIN 5
 #define PIXEL_COUNT 288
+#define BUFFER_SIZE 8
+
+enum states { CONTROL_OPEN, LEFT_CONTROL, LEFT_SENDING, RIGHT_CONTROL, RIGHT_SENDING };
 
 PS2Keyboard keyboard;
 int i=0;
@@ -26,8 +29,10 @@ int i=0;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 SerialLCD lcd(2,16,0x28,I2C);
-char buffer[9];
-int charPointer=0;
+char buffer[BUFFER_SIZE + 1];
+int charPointer = 0;
+int sendShift = 0;
+int state = CONTROL_OPEN;
 
 void setup() {
   // Initialize LCD module
@@ -43,53 +48,59 @@ void setup() {
   lcd.clear();
   lcd.setContrast(40);
   
-  for (int i=0; i<9; i++) buffer[i]=0;
+  for (int i=0; i<=BUFFER_SIZE; i++) buffer[i]=0;
 }
 
 void loop() {
+  switch (state) {
+    case CONTROL_OPEN:
+    case RIGHT_CONTROL: rightControl(); break;
+    case RIGHT_SENDING: rightSending(); break;
+  }
+//  delay(20);
+}
+
+void rightControl() {
   if (keyboard.available()) {
     char c = keyboard.read();
-//    strip.clear();
-//    strip.setPixelColor(c, Wheel(c));
-//    strip.show();
+
     if (c == PS2_BACKSPACE) {
       if (charPointer > 0)
         buffer[--charPointer] = 0;
+    } else if (c == PS2_ENTER) {
+      sendShift = 0;
+      state = RIGHT_SENDING;
     } else {
-      if (charPointer < 8)
+      if (charPointer < BUFFER_SIZE)
         buffer[charPointer++] = c;
     }
+
     lcd.clear();
     lcd.home();
     lcd.print(buffer);
     
     bitLights(buffer);
   }
-//  i = (i +1)% 288;
-//  delay(20);
+}
+
+void rightSending() {
+  sendShift++;
+  if (sendShift + 8 * BUFFER_SIZE > PIXEL_COUNT) {
+    state = CONTROL_OPEN;
+    return;
+  }
+  bitLights(buffer);
 }
 
 void bitLights(char *buffer) {
   strip.clear();
   for (int i=0; i<8; i++) {
     for (int j=0; j<8; j++) {
-      int pos = i*8+j;
+      int pos = sendShift + i*8+j;
       strip.setPixelColor( pos, (buffer[i] >> j) & 0x1 ? Wheel(pos+charPointer*6) : strip.Color(0,0,0) );
     }
   }
   strip.show();
-}
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
 }
 
 // Input a value 0 to 255 to get a color value.
